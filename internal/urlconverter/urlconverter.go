30d42568fd20e9342cb174cc000f0faf135c8e0c
denisfd <fedorchenko999@gmail.com>
2020-04-19 22:38:55 +0300

    Add urlconverter

---
+++ internal/urlconverter/urlconverter.go
@@ -0,0 +1,42 @@
+package urlconverter
+
+import (
+	"fmt"
+)
+
+var elems = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+// IDToShortURL generates short URL based on integer id.
+func IDToURL(id uint64) []byte {
+	var url []byte
+
+	for id > 0 {
+		url = append(url, elems[id%62])
+		id /= 62
+	}
+
+	return url
+}
+
+// URLToID restores id from short URL.
+func URLToID(url []byte) (uint64, error) {
+	var id uint64
+
+	for _, b := range url {
+		switch {
+		case 'a' <= b && b <= 'z':
+			id = id*62 + uint64(b-'a')
+
+		case 'A' <= b && b <= 'Z':
+			id = id*62 + uint64(b-'A') + 26
+
+		case '0' <= b && b <= '9':
+			id = id*62 + uint64(b-'0') + 52
+
+		default:
+			return id, fmt.Errorf("URL contains unsupported symbol %q", b)
+		}
+	}
+
+	return id, nil
+}
